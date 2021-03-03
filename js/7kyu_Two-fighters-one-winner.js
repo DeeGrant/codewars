c3371c56b7685a8479c2706e8f3104a05b0d828f
Walter Grant <WalterGrant@gmail.com>
2021-03-03 11:20:18 -0600

    more oop solution

--- js/7kyu_Two-fighters-one-winner.js
+++ js/7kyu_Two-fighters-one-winner.js
@@ -7,23 +7,56 @@ function Fighter(name, health, damagePerAttack) {
     this.toString = function() { return this.name; }
 }
 
+// more oop solution ---------------------------------------------------------------------------------------------------
+// v
+Fighter.prototype.attack = function (defender) {
+    defender.health = defender.health - this.damagePerAttack
+}
+
 function declareWinner(fighter1, fighter2, firstAttacker) {
-    let oneIsAttacker = fighter1.toString() === firstAttacker
+    let isOneAttacking = fighter1.toString() === firstAttacker
     // fight
     do {
-        if (oneIsAttacker) {
-            attack(fighter1, fighter2)
-        } else {
-            attack(fighter2, fighter1)
-        }
-        oneIsAttacker = !oneIsAttacker
+        isOneAttacking ?
+            fighter1.attack(fighter2)
+        :
+            fighter2.attack(fighter1)
+
+        isOneAttacking = !isOneAttacking
     } while (fighter1.health > 0 && fighter2.health > 0)
 
     // winner
-    oneIsAttacker = !oneIsAttacker
-    return oneIsAttacker ? fighter1.toString() : fighter2.toString()
+    isOneAttacking = !isOneAttacking
+    return isOneAttacking ? fighter1.toString() : fighter2.toString()
 }
+// ^
+// more oop solution ---------------------------------------------------------------------------------------------------
 
-function attack(attacker, defender) {
-    defender.health = defender.health - attacker.damagePerAttack
-}
+
+
+// first solution ------------------------------------------------------------------------------------------------------
+// v
+
+// function declareWinner(fighter1, fighter2, firstAttacker) {
+//     let oneIsAttacker = fighter1.toString() === firstAttacker
+//     // fight
+//     do {
+//         if (oneIsAttacker) {
+//             attack(fighter1, fighter2)
+//         } else {
+//             attack(fighter2, fighter1)
+//         }
+//         oneIsAttacker = !oneIsAttacker
+//     } while (fighter1.health > 0 && fighter2.health > 0)
+//
+//     // winner
+//     oneIsAttacker = !oneIsAttacker
+//     return oneIsAttacker ? fighter1.toString() : fighter2.toString()
+// }
+//
+// function attack(attacker, defender) {
+//     defender.health = defender.health - attacker.damagePerAttack
+// }
+
+// ^
+// first solution ------------------------------------------------------------------------------------------------------
